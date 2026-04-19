20e598c9904fcf9b01b692b1ffd248603f10323e
Sunny kumar <sk5962779@gmail.com>
2026-04-20 05:24:52 +0530

    final project ready for deployment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -7,7 +7,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => {
