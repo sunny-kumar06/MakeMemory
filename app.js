b6e3c54829b4a6ff4f039e733f61bf0e1d2b28d4
Sunny kumar <sk5962779@gmail.com>
2026-04-20 05:37:05 +0530

    .env improve

--- app.js
+++ app.js
@@ -79,20 +79,8 @@ app.delete("/listings/:id", async (req, res) => {
   res.redirect("/listings");
 });
 
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
 
-//   await sampleListing.save();
-//   console.log("sample was saved");
-//   res.send("successful testing");
-// });
 
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+app.listen(process.env.PORT, () => {
+  console.log("server is listening to port", process.env.PORT);
 });
